b932c4a715abf0e2b32b1883533ca6f7ca1664a5
treere <tomasiandrea.at@gmail.com>
2021-12-29 12:45:37 +0100

    Partial gitea integration

--- lisp/forge-gitea.el
+++ lisp/forge-gitea.el
@@ -43,6 +43,116 @@
    (create-pullreq-url-format :initform "https://%h/%o/%n/pulls") ; sic
    (pullreq-refspec :initform "+refs/pull/*/head:refs/pullreqs/*")))
 
+(cl-defmethod forge--pull ((repo forge-gitea-repository) until)
+  (let ((cb (let ((buf (and (derived-mode-p 'magit-mode)
+                            (current-buffer)))
+                  (dir default-directory)
+                  (val nil))
+              (lambda (cb &optional v)
+                (when v (if val (push v val) (setq val v)))
+                (let-alist val
+                  (cond
+                   ((not val)
+                    (forge--fetch-repository repo cb))
+                   ((not (assq 'assignees val))
+                    (forge--fetch-assignees repo cb))
+                   ;; ((not (assq 'forks val))
+                    ;; (forge--fetch-forks repo cb))
+                   ;; ((not (assq 'labels val))
+                    ;; (forge--fetch-labels repo cb))
+                   ;; ((and .issues_enabled
+                         ;; (not (assq 'issues val)))
+                    ;; (forge--fetch-issues repo cb until))
+                   ;; ((and .merge_requests_enabled
+                         ;; (not (assq 'pullreqs val)))
+                    ;; (forge--fetch-pullreqs repo cb until))
+                   (t
+                    (forge--msg repo t t   "Pulling REPO")
+                    (forge--msg repo t nil "Storing REPO")
+                    (emacsql-with-transaction (forge-db)
+                      (forge--update-repository repo val)
+                      (forge--update-assignees  repo .assignees)
+                      ;; (forge--update-labels     repo .labels)
+                      ;; (dolist (v .issues)   (forge--update-issue repo v))
+                      ;; (dolist (v .pullreqs) (forge--update-pullreq repo v))
+                      ;; (oset repo sparse-p nil)
+                      )
+                    (forge--msg repo t t "Storing REPO")
+                    (unless (oref repo selective-p)
+                      (forge--git-fetch buf dir repo)))))))))
+    (funcall cb cb)))
+
+(cl-defmethod forge--fetch-repository ((repo forge-gitea-repository) callback)
+  (forge--gtea-get repo "/repos/tre3ere/test" nil
+    :callback (lambda (value _headers _status _req)
+                (cond ((oref repo selective-p)
+                       (setq value (append '((assignees) (forks) (labels)
+                                             (issues) (pullreqs))
+                                           value)))
+                      ((magit-get-boolean "forge.omitExpensive")
+                       (setq value (append '((assignees) (forks) (labels))
+                                           value))))
+                (funcall callback callback value))))
+
+(cl-defmethod forge--update-repository ((repo forge-gitea-repository) data)
+  (let-alist data
+    (oset repo created        .created_at)
+    (oset repo updated        .updated_at)
+    (oset repo pushed         nil)
+    (oset repo parent         .parent)
+    (oset repo description    .description)
+    (oset repo homepage       nil)
+    (oset repo default-branch .default_branch)
+    (oset repo archived-p     .archived)
+    (oset repo fork-p         .fork)
+    (oset repo locked-p       nil)
+    (oset repo mirror-p       .mirror)
+    (oset repo private-p      .private)
+    (oset repo issues-p       .has_issues)
+    (oset repo wiki-p         .has_enabled)
+    (oset repo stars          .star_count)
+    (oset repo watchers       .watchers_count)))
+
+
+(cl-defmethod forge--fetch-assignees ((repo forge-gitea-repository) callback)
+  (forge--gtea-get repo "/repos/tre3ere/test/assignees"
+    '((per_page . 100))
+    :unpaginate t
+    :callback (lambda (value _headers _status _req)
+                (message "Assignees %S" value)
+                (funcall callback callback (cons 'assignees value)))))
+
+(cl-defmethod forge--update-assignees ((repo forge-gitea-repository) data)
+  (oset repo assignees
+        (with-slots (id) repo
+          (mapcar (lambda (row)
+                    (let-alist row
+                      ;; For other forges we don't need to store `id'
+                      ;; but here we do because that's what has to be
+                      ;; used when assigning issues.
+                      (list (forge--object-id id .id)
+                            .username
+                            .full_name
+                            .id)))
+                  data))))
+
+
+(cl-defun forge--gtea-get (obj resource
+                               &optional params
+                               &key query payload headers
+                               silent unpaginate noerror reader
+                               host callback errorback)
+  (declare (indent defun))
+  (gtea-get (if obj (forge--format-resource obj resource) resource)
+            params
+            :host (or host (oref (forge-get-repository obj) apihost))
+            :auth 'forge
+            :query query :payload payload :headers headers
+            :silent silent :unpaginate unpaginate
+            :noerror noerror :reader reader
+            :callback callback
+            :errorback (or errorback (and callback t))))
+
 ;;; _
 (provide 'forge-gitea)
 ;;; forge-gitea.el ends here
